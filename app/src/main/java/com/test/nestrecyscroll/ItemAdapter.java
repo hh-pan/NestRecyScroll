d6fef9bf7c28134a83f668bf13cf7c83c9788314
526437186@qq.com <panpan123>
2019-07-30 17:57:33 +0800

    init

---
+++ app/src/main/java/com/test/nestrecyscroll/ItemAdapter.java
@@ -0,0 +1,43 @@
+package com.test.nestrecyscroll;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Pan on 2019/7/30.
+ * Desc:
+ */
+public class ItemAdapter extends RecyclerView.Adapter {
+
+    private Context mContext;
+
+    public ItemAdapter(Context context) {
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new MyViewHolder(View.inflate(mContext,R.layout.recy_item,null));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 20;
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
---
+++ app/src/main/java/com/test/nestrecyscroll/PageFragment.java
@@ -0,0 +1,40 @@
+package com.test.nestrecyscroll;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Pan on 2019/7/30.
+ * Desc:
+ */
+public class PageFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return View.inflate(getContext(), R.layout.fragment_page, null);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ItemAdapter adapter = new ItemAdapter(getContext());
+        RecyclerView recyclerView = view.findViewById(R.id.recy);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+
+    }
+}
